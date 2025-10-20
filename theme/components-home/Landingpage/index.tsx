5fd8405a60aec67d9e5b394c07a0db4cdfc840dd
Cezar Augusto <boss@cezaraugusto.net>
2025-10-20 01:37:54 -0300

    Add Lambdatest ref

--- theme/components-home/Landingpage/index.tsx
+++ theme/components-home/Landingpage/index.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
+import Sponsors from "./Sponsors";
 
 const LandingPage = () => {
   return (
     <main className="max-w-[1280px] px-6 m-auto">
       <Hero />
+      <Sponsors />
     </main>
   );
 };
