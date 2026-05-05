87b5921226157b71dfecbc72c0ae0f6c1684840f
Cezar Augusto <boss@cezaraugusto.net>
2026-05-05 05:54:40 -0300

    Copy tweaks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,41 @@
-import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import onlyWarn from 'eslint-plugin-only-warn'
-import prettierPlugin from 'eslint-plugin-prettier'
-import pluginReact from 'eslint-plugin-react'
-import pluginReactHooks from 'eslint-plugin-react-hooks'
-import turboPlugin from 'eslint-plugin-turbo'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import prettierPlugin from "eslint-plugin-prettier";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import turboPlugin from "eslint-plugin-turbo";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**', '.pnpm-store/**', 'coverage/**']
+    ignores: ["dist/**", "node_modules/**", ".pnpm-store/**", "coverage/**"],
   },
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
   {
     plugins: {
-      turbo: turboPlugin
+      turbo: turboPlugin,
     },
     rules: {
-      'turbo/no-undeclared-env-vars': 'warn'
-    }
+      "turbo/no-undeclared-env-vars": "warn",
+    },
   },
   {
     plugins: {
-      prettier: prettierPlugin
+      prettier: prettierPlugin,
     },
     rules: {
-      'prettier/prettier': 'error'
-    }
+      "prettier/prettier": "error",
+    },
   },
   {
     plugins: {
-      onlyWarn
-    }
+      onlyWarn,
+    },
   },
   pluginReact.configs.flat.recommended,
   {
@@ -43,29 +43,29 @@ export default [
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
         ...globals.browser,
-        ...globals.serviceworker
-      }
-    }
+        ...globals.serviceworker,
+      },
+    },
   },
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
       globals: {
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   {
     plugins: {
-      'react-hooks': pluginReactHooks
+      "react-hooks": pluginReactHooks,
     },
     settings: {
-      react: {version: 'detect'}
+      react: { version: "detect" },
     },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off'
-    }
-  }
-]
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
